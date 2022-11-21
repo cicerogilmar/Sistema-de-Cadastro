9b953cda1cfe134ec296f906ac224d1f4103314a
Cícero Gilmar de Oliveira Arruda <cicerogilmar@gmail.com>
2022-11-20 23:06:49 -0300

    alterando novamente

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 console.log("testando git")
-console.lgo("testando git 02")
+console.log("testando git 02")
+console.log("testando nova alteração")
